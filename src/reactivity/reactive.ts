af6b1c1f98bf09947d1f0082cfc81783fb7d8747
LinGuiHao <513279340@qq.com>
2022-12-18 14:50:58 +0800

    add readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,20 +1,20 @@
-import { track,trigger } from "./effect"
+import { mutableHandlers, readonlyHandlers } from "./baseHandlers"
+import { track, trigger } from "./effect"
+
+
+
+
 export function reactive(raw) {
-    return new Proxy(raw, {
-        get(target, key) {
-            const res =Reflect.get(target,key)
+    return createActiveObject(raw, mutableHandlers)
+}
 
-            //依赖收集 
-            track(target,key)
-            return res
-        } ,
-        set(target,key,value){
-            const res= Reflect.set(target,key,value )
+export function readonly(raw) {
 
+    return createActiveObject(raw, readonlyHandlers)
 
-            // 触发依赖 
-            trigger(target,key)
-            return res
-        }
-    })
+}
+
+function createActiveObject(raw: any, baseHandlers) {
+    return new Proxy(raw, baseHandlers)
+
 }
