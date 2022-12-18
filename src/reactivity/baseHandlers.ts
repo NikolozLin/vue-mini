95f32491666a78f10fef0c90530507d052a31371
LinGuiHao <513279340@qq.com>
2022-12-18 15:08:45 +0800

    add Fn ( isReadonly & isReactive)

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,5 +1,5 @@
-import { track, trigger } from "./effect"
-
+import { track, trigger, } from "./effect"
+import { ReactiveFlags } from "./reactive";
 
 const get = createGetter();
 const set = createSetter();
@@ -8,6 +8,12 @@ const readonlyGet = createGetter(true);
 function createGetter(isReadonly = false) {
     return function get(target, key) {
         const res = Reflect.get(target, key)
+        if (key === ReactiveFlags.IS_REACTIVE) {
+
+            return !isReadonly
+        } else if (key === ReactiveFlags.IS_READONLY) {
+            return isReadonly
+        }
 
         if (!isReadonly) {
             //依赖收集 
