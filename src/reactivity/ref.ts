52ba310158a9ecaca68cfeaa79f0e3082f809f48
linguihao <513279340@qq.com>
2022-12-22 14:45:18 +0800

    add isRef unRef proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
     private _rawValue: any
     private _value: any
     public dep
+    public __v_isRef = true
 
     constructor(value) {
         this._rawValue = value;
@@ -15,19 +16,19 @@ class RefImpl {
 
     get value() {
         trackRefValue(this)
-        return this._value; 
+        return this._value;
     }
     set value(newValue) {
- 
+
         // 如果是值是对象类型 需要对比原始值
         if (hasChanged(newValue, this._rawValue)) return;
-        this._rawValue= newValue;
+        this._rawValue = newValue;
         this._value = convert(newValue);
         triggerEffect(this.dep)
     }
 }
 
-function convert(value){
+function convert(value) {
     return isObject(value) ? reactive(value) : value;
 }
 
@@ -39,5 +40,36 @@ function trackRefValue(ref) {
 
 export function ref(value) {
     return new RefImpl(value)
+
+}
+
+
+export function isRef(ref) {
+    return !!ref.__v_isRef
+}
+
+export function unRef(ref) {
+
+    if (isRef(ref)) return ref._rawValue
+    return ref
+
+}
+
+export function proxyRefs(objectWithRefs) {
+
+    return new Proxy(objectWithRefs, {
+        get(target, key) {
+            return unRef(Reflect.get(target, key))
+        },
+
+        set(target, key, newValue) {
+            if (isRef(target[key]) && !isRef(newValue)) {
+                return target[key].value = newValue;
+            } else {
+                return Reflect.set(target, key, newValue)
+            }
+
+        },
+    })
 
 }
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from "../effect";
-import { ref } from "../ref";
+import { reactive } from "../reactive";
+import { isRef, proxyRefs, ref, unRef } from "../ref";
 
 describe('ref', () => {
     it('base test', () => {
@@ -10,35 +11,74 @@ describe('ref', () => {
     it('should be reactive', () => {
         const a = ref(1);
         let dummy;
-        let calls=0;
-        effect(()=>{
+        let calls = 0;
+        effect(() => {
             calls++;
-            dummy=a.value;
+            dummy = a.value;
         })
         expect(calls).toBe(1);
         expect(dummy).toBe(1);
-        a.value =2;
+        a.value = 2;
         expect(calls).toBe(2);
         expect(dummy).toBe(2);
         // some value shoule not trigger
-        a.value = 2 
+        a.value = 2
         expect(calls).toBe(2);
         expect(dummy).toBe(2);
-        
+
     });
 
     it('should make nested properties reactive', () => {
-        const a =ref({
-            count:1
+        const a = ref({
+            count: 1
         })
         let dummy;
-        effect(()=>{
-            dummy=a.value.count
+        effect(() => {
+            dummy = a.value.count
         })
         expect(dummy).toBe(1);
         a.value.count = 2;
         expect(dummy).toBe(2);
 
+    });
+
+
+    it('ifRef ', () => {
+
+        const a = ref(1);
+        const user = reactive({
+            age: 1
+        })
+        expect(isRef(a)).toBe(true)
+        expect(isRef(1)).toBe(false)
+        expect(isRef(user)).toBe(false)
+    });
+
+    it('unRef ', () => {
+
+        const a = ref(1);
+        expect(unRef(a)).toBe(1)
+        expect(unRef(1)).toBe(1)
+    });
+    it('proxyRefs', () => {  
+        //    proxyRefs用途：让 Vue3 中的template 取值不需要像setup里面通过 .value 取值      
+        const user ={
+            age:ref(10),
+            name:'tom'
+        }
+        const proxyUser= proxyRefs(user);
+        expect(user.age.value).toBe(10);
+        expect(proxyUser.age).toBe(10);
+        expect(proxyUser.name).toBe("tom");
+
+        proxyUser.age=20
+        expect(user.age.value).toBe(20);
+        expect(proxyUser.age).toBe(20);
+
+        proxyUser.age=ref(10)
+        expect(user.age.value).toBe(10);
+        expect(proxyUser.age).toBe(10);
+        
     });
 
 });
