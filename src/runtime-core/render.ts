5947f04967921d28af72849d0a24126bc963da87
linguihao <513279340@qq.com>
2022-12-28 17:46:46 +0800

    add  ShapeFlage 进行vnode 类型判断

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared/index"
+import { ShapeFlags } from "../shared/ShapeFlags"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -8,9 +9,11 @@ export function render(vnode, container) {
 
 function patch(vnode, container) {
     // TODO 判断vnode 是不是 element
-    if (typeof vnode.type === 'string') {
+    const {shapeFlag } = vnode;
+    if (shapeFlag&ShapeFlags.ELEMENT) {
+        // 处理元素
         processElement(vnode, container) 
-    } else if (isObject(vnode.type)) {
+    } else  if (shapeFlag&ShapeFlags.STATEFUL_COMPONENT){
         // 处理组件
         processCpmponent(vnode, container)
     }
@@ -24,11 +27,11 @@ function mountElement(vnode: any, container: any) {
 
     const el = (vnode.el=document.createElement(vnode.type))
     // 两种可能 string array
-    const { children } = vnode;
-    if(typeof children ==='string'){
+    const { children,shapeFlag } = vnode;
+    if(shapeFlag&ShapeFlags.TEXT_CHILDREN){
 
         el.textContent = children;
-    }else if(Array.isArray(children)){
+    }else if(shapeFlag&ShapeFlags.ARRAY_CHILDREN){
         mountChildren(vnode,el)
        
     }
